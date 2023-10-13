9ecd91be27d65128c298ab66373a943d54f45d35
krishnachaitanya7 <chaitanyaradon89@gmail.com>
2023-10-12 20:57:01 -0700

    sync

--- my_yolo.py
+++ my_yolo.py
@@ -1,18 +1,17 @@
-import sys
-from scripts.project_constants import YOLO_CHECKPOINT
-
-# from scripts.panda_moveit_library import FrankaOperator
-from scripts.project_constants import PANDA_HOME_JOINTS_VISION, D405_REALSENSE_CAMERA_ID
 import torch
 from ultralytics import YOLO
-import sys
 import numpy as np
 import torch
 import cv2
 from time import sleep
 import pyrealsense2
+import sys
 
-# import rospy
+sys.path.append("/usr/lib/python3/dist-packages")  # For rospkg dependency
+from scripts.project_constants import YOLO_CHECKPOINT
+from scripts.panda_moveit_library import FrankaOperator
+from scripts.project_constants import PANDA_HOME_JOINTS_VISION, D405_REALSENSE_CAMERA_ID
+import rospy
 
 
 def main():
@@ -27,13 +26,17 @@ def main():
     config.enable_device(D405_REALSENSE_CAMERA_ID)
     config.enable_stream(pyrealsense2.stream.depth, 640, 480, pyrealsense2.format.z16, 30)
     config.enable_stream(pyrealsense2.stream.color, 640, 480, pyrealsense2.format.bgr8, 30)
+    align = pyrealsense2.align(pyrealsense2.stream.color)
     pipeline.start(config)
     # write a one line function to round the tensor to nearest integer
     round_tensor = lambda x: round(float(x.data))
     while 1:
         frames = pipeline.wait_for_frames()
+        # align the depth and color frames
+        frames = align.process(frames)
         color_frame = frames.get_color_frame()
         depth_frame = frames.get_depth_frame()
+        depth_intrin = depth_frame.profile.as_video_stream_profile().intrinsics
         color_image = np.asanyarray(color_frame.get_data())
         depth_frame = np.asanyarray(depth_frame.get_data())
         # cv2 resize image to 640, 640
@@ -74,25 +77,27 @@ def main():
                         (0, 255, 0),
                         2,
                     )
-                    if class_name == "Tomato Sauce":
+                    if class_name == "Mushrooms":
                         break
-        # mask depth except the bounding box
-        mask = np.zeros_like(depth_frame)
-        mask[
-            round_tensor(bb_box[1]) : round_tensor(bb_box[3]), round_tensor(bb_box[0]) : round_tensor(bb_box[2])
-        ] = depth_frame[
-            round_tensor(bb_box[1]) : round_tensor(bb_box[3]), round_tensor(bb_box[0]) : round_tensor(bb_box[2])
-        ]
-        # Find the pixel coordinates of the lowest value other than zero in the mask
+        # Method 1: Just find the closest point depth from camera (Doesn't work well)
+        # # mask depth except the bounding box
+        # mask = np.zeros_like(depth_frame)
+        # mask[
+        #     round_tensor(bb_box[1]) : round_tensor(bb_box[3]), round_tensor(bb_box[0]) : round_tensor(bb_box[2])
+        # ] = depth_frame[
+        #     round_tensor(bb_box[1]) : round_tensor(bb_box[3]), round_tensor(bb_box[0]) : round_tensor(bb_box[2])
+        # ]
+        # # Find the pixel coordinates of the lowest value other than zero in the mask
 
         # highest_pixel = np.where(mask == np.min(mask[np.nonzero(mask)]))
         # cv2.circle(color_image, (highest_pixel[1][0], highest_pixel[0][0]), 10, (0, 255, 0), -1)
-        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-        # Find the convex hull for each contour
-        for contour in contours:
-            hull = cv2.convexHull(contour)
-            cv2.drawContours(color_image, [hull], -1, (255, 255, 255), 2)
+        # Method 2: Find the 3D points of entire bounding box and find the closest point
+        # Get the 3D points of the bounding box
+        # Get the 3D points of the bounding box
+        bb_box_3d = []
+        for i in range(round_tensor(bb_box[0]), round_tensor(bb_box[1] * (480.0 / 640.0))):
+            for j in range(round_tensor(bb_box[2]), round_tensor(bb_box[3] * (480.0 / 640.0))):
+                bb_box_3d.append(pyrealsense2.rs2_deproject_pixel_to_point(depth_intrin, [i, j], depth_frame[j][i]))
 
         cv2.imshow("image", color_image)
         # sleep for 1 ms
